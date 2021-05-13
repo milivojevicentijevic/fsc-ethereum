a8f85d13a0075d5f80c491727d4e753a5aca4d96
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-05-13 19:23:36 +0200

    header, welcome

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// typed.js
+var typed = new Typed('.element', {
+    // Waits 1000ms after typing "First"
+    strings: ['Arabic', 'Chinese', 'Finnish', 'Hungarian', 'Dutch', 'Italian', 'English', 'French', 'Japanese'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    smartBackspace: true,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: false
+  });
